afba62cd3b1c7e0096c07c4b446eb8d12527842f
Kiran <kirankakde2128@gmail.com>
2026-02-20 21:35:54 +0530

    My First Commit

--- a1.js
+++
@@ -1,4 +0,0 @@
-/* my first js program */
-
-var name = "Rajesh Gupta"
-console.log("Hello and Welcome" + name)
--- a2.js
+++
@@ -1,6 +0,0 @@
-/* area of triangle */
-
-var ht = 12
-var ba = 8
-var area = (ht * ba) / 2
-console.log("Area of triangle is" + area)
